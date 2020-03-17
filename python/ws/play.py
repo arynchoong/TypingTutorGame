b153d5b443bf042aeaf5e33634ef56be8603e791
arynchoong <arynchoong@hotmail.com>
2020-03-18 07:38:13 +0800

    Update play.py

--- python/ws/play.py
+++ python/ws/play.py
@@ -1,36 +1,167 @@
 # play.py
 import pygame
+from objects import GameObjects
 
 # Colours    R    G    B
 BLACK   = (  0,   0,   0)
-GREEN   = (  0, 255,   0)
+BLUE        = (  0,   0, 128)
+LIGHTBLUE   = (135, 206, 235)
+WHITE       = (255, 255, 255)
+GREEN       = (  0, 200,   0)
+YELLOW      = (255, 255,   0)
+
+MARGINX = 6
 
 class GamePlay():
     def __init__(self, screen):
+        pygame.init()
         self.screen = screen
-        self.font = pygame.font.Font('Consolas Bold.ttf', 24)
+        self.disp_surf = pygame.Surface(self.screen.get_size()).convert()
+        self.width, self.height = screen.get_size()
+        self.font =  pygame.font.Font('Consolas Bold.ttf', 16)
+        self.game_objects = GameObjects(self.width, self.height)
+        self.playing = True
+        self.last_add = 0 # time of adding word
+        self.add_timeout = 2000 # 2 seconds
+        self.fps = 25
+        self.fpsclock = pygame.time.Clock()
+        self.ydelta = 1
+        self.typing_flag = False
+        self.score = 0
+        self.keyhit = None
+        self.level = 1
+        self.typed_count = 0
         
     def execute(self):
-        self.render()
+        while self.playing:
+            self.check_events()
+            self.loop()
+            self.render()
+            self.fpsclock.tick(self.fps)
+        return self.score
+    
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.TEXTINPUT:
+                self.keyhit = event.text
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE:
+                    self.playing = False
+        return
+    
+    def loop(self):
+        self.check_keyhit()
+        if self.game_objects.is_gameover():
+            self.playing = False
+            return
+            
+        if self.game_objects.move(self.ydelta):
+            self.typing_flag = False
+        self.add_word()
         return
         
     def render(self):
         '''Render surface'''
-        # Create surface
-        disp_surf = pygame.Surface(self.screen.get_size())
-        self.disp_surf = disp_surf.convert()
-        self.disp_surf.fill(BLACK) # Fill background
-        # set the text for display
-        self.draw_text("Playing ...", GREEN, self.disp_surf.get_rect().center)
+        self.disp_surf.fill(BLUE) # Fill background
+        self.draw_score()
+        
+        # set the words for display
+        for word in self.game_objects.game_words:
+            if word.typedidx > -1:
+                typed_text = word.text[:word.typedidx+1]
+                untyped_text = ' '*(word.typedidx+1) + word.text[word.typedidx+1:]
+                self.draw_word(untyped_text, WHITE, word.coord())
+                self.draw_word(typed_text, GREEN, word.coord())
+            else:
+                self.draw_word(word.text, WHITE, word.coord())
+        
+        # cityline
+        base_y = self.height - 30
+        start_x = MARGINX
+        start_y = base_y
+        for block in self.game_objects.cityline:
+            end_x = start_x + 10
+            end_y = base_y + block.get_height()
+            if block.get_flag():
+                pygame.draw.lines(self.disp_surf, LIGHTBLUE, False,
+                    [(start_x,start_y), (start_x,end_y), (end_x,end_y)], 3)
+            start_y = end_y
+            start_x = end_x
+        
         # Blit everything to screen
         self.screen.blit(self.disp_surf, (0,0))
         pygame.display.flip()
         return
     
-    def draw_text(self, text, colour, center):
+    def draw_word(self, text, colour, coord):
         # Display some text
         textsurf = self.font.render(text, True, colour)
         textrect = textsurf.get_rect()
-        textrect.center = center
+        textrect.topleft = coord
         self.disp_surf.blit(textsurf, textrect)
+        return
+        
+    def add_word(self):
+        if self.game_objects.is_gameover():
+            return
+        # Check if valid to add
+        if not self.game_objects.game_words or (
+            (pygame.time.get_ticks() - self.last_add) > self.add_timeout):
+            if self.game_objects.add_word(self.level):
+                self.last_add = pygame.time.get_ticks()
+        return
+    
+    def check_keyhit(self):
+        if not self.keyhit:
+            return
+        if self.typing_flag:
+            for word in self.game_objects.game_words:
+                if word.typedidx != -1:
+                    if word.text[word.typedidx+1] == self.keyhit:
+                        if word.typed():
+                            self.typing_flag = False
+                            self.add_score(len(word))
+                    else:
+                        word.typed_reset()
+                        self.typing_flag = False
+        else:
+            for word in self.game_objects.game_words:
+                if word.text[0] == self.keyhit:
+                    if word.typed():
+                        self.add_score(1)
+                    else:
+                        self.typing_flag = True
+                    break
+        self.game_objects.clean_up()
+        self.keyhit = None
+        return
+    
+    def add_score(self, score):
+        self.score += score
+        self.typed_count += 1
+        
+        # Level up
+        if self.typed_count >= 5:
+            self.level += 1
+            self.typed_count = 0
+            # Increase difficulty when leveling up
+            if self.fps < 60:
+                self.fps += 3
+            elif self.ydelta < 3:
+                self.ydelta +=1
+            elif self.add_timeout > 900:
+                self.add_timeout -= 150
+        return
+
+    def draw_score(self):
+        # draw status bar background
+        pygame.draw.rect(self.disp_surf, BLACK, (0,0,self.width,20), 0)
+        # draw score text
+        text = 'Score: %s' % self.score
+        self.draw_word(text, YELLOW, (self.width - 150, 2))
+        # draw level text
+        text = 'Level: %s' % self.level
+        self.draw_word(text, YELLOW, (MARGINX, 2))
         return
